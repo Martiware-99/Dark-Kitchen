e183f0dd45ee172aa3bb8cfe269ad92f37e894e3
Martiware-99 <martin.neyns.pro@gmail.com>
2021-09-21 09:05:34 +0200

    improved js

--- script.js
+++ script.js
@@ -1,36 +1,39 @@
 const pastadore = [{
     menu: 'pasta bolognese',
     ingredients: ['penne, spaghetti ou tagliatelle', 'sauce tomate', 'viande hâchée'],
-    vegan: 'false',
+    vegan: false,
     description: 'Du vrai fait maison',
     prix: 6.50,
+    image: "https://www.allesoveritaliaanseten.nl/wp-content/uploads/2015/03/Pasta-Bolognese.jpg",
 },
 {
     menu: 'carbonara',
     ingredients: ['penne, spaghetti ou tagliatelle', 'jambon', 'crème fraîche'],
-    vegan: 'false',
+    vegan: false,
     description: 'Maestro des pâtes',
     prix: 5.50,
+    image: "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/recipe-image-legacy-id-1001491_11-2e0fa5c.jpg",
 },
 {
     menu: 'carbonara vegan',
     ingredients: ['penne, spaghetti ou tagliatelle', 'brocoli', 'crème fraîche'],
-    vegan: 'true',
+    vegan: true,
     description: 'International healthy pasta',
     prix: 8.00,
+    image: "https://choosingchia.com/jessh-jessh/uploads/2020/09/vegan-carbonara-4.jpg",
 },
 {
     menu: 'tagliatelle tricolore',
     ingredients: ['tagliatelle', 'courgette', 'basilic'],
-    vegan: 'true',
+    vegan: true,
     description: 'Idéal pour le soir',
     prix: 8.00,
+    image: "https://images.eatsmarter.com/sites/default/files/styles/576x432/public/tricolore-tagliatelle-with-prawns-505201.jpg",
 }
 ];
 
-function createCard(dishName) {
+function createCard(dishes) {
     let cards = document.createElement("div")
-    cards.className = albumTitle.className
     cards.className = "card"
     document.querySelector(".container").appendChild(cards)
     
@@ -42,48 +45,53 @@ function createCard(dishName) {
     back.className = "back"
     cards.appendChild(back)
     
-    let main = document.createElement("main")
-    back.appendChild(main)
-    
-    let header = document.createElement("header")
-    main.appendChild(header)
-    
-    let article = document.createElement("article")
-    main.appendChild(article)
     
     let footer = document.createElement("footer")
-    main.appendChild(footer)
-    
-    let title = document.createElement("div")
-    title.className = "title"
-    header.appendChild(title)
-    
-    let date = document.createElement("div")
-    date.className = "date"
-    header.appendChild(date)
-    
-    let albumName = document.createElement("h1")
-    title.appendChild(albumName)
-    albumName.innerText = albumTitle.album  
+    back.appendChild(footer)
 
-    let albumDate = document.createElement("p")
-    date.appendChild(albumDate)
-    albumDate.innerText = albumTitle.year
+    let plat = document.createElement("p")
+    footer.appendChild(plat)
+    plat.innerText = dishes.menu
 
-    let albumAuthor = document.createElement("p")
-    title.appendChild(albumAuthor)
-    albumAuthor.innerText = albumTitle.artist
+    let price = document.createElement("p")
+    footer.appendChild(price)
+    price.className = "prix"
+    price.innerText = dishes.prix
 
-    let albumdescription = document.createElement("p")
-    article.appendChild(albumdescription)
-    albumdescription.innerText = albumTitle.description
 
-    let albumGenre = document.createElement("p")
-    footer.appendChild(albumGenre)
-    albumGenre.innerText = albumTitle.genre
-    
-    front.style.backgroundImage = "url('"+ albumTitle.cover +"')";
+    let platBack = document.createElement("h3")
+    back.appendChild(platBack)
+    platBack.innerText = dishes.menu
 
-    back.style.backgroundColor = albumTitle.color;
+    let description = document.createElement("p")
+    back.appendChild(description)
+    description.innerText = dishes.description
 
+    let ingrédients = document.createElement("h4")
+    back.appendChild(ingrédients)
+    ingrédients.innerText = "liste d'ingrédient"
+
+    let liste = document.createElement("ul")
+    back.appendChild(liste)
+
+    let listItem1 = document.createElement("li")
+    liste.appendChild(listItem1)
+    listItem1.innerText  = dishes.ingredients[0]
+
+    let listItem2 = document.createElement("li")
+    liste.appendChild(listItem2)
+    listItem2.innerText  = dishes.ingredients[1]
+
+    let listItem3 = document.createElement("li")
+    liste.appendChild(listItem3)
+    listItem3.innerText  = dishes.ingredients[2]
+
+
+    front.style.backgroundImage = "url('"+ dishes.image +"')";
+
+}
+
+for (let dishes of pastadore){
+    console.log(dishes);
+    createCard(dishes)
 }
