4657889fc3d15af662be73b2fbd2597c41d423fc
Dyl-Richardson <dylanrichardson1606@gmail.com>
2021-09-21 21:44:51 +0200

    update

--- js/script.js
+++ js/script.js
@@ -178,7 +178,7 @@ function createCard(dishes) {
 
 }
 
-
+/* Filter */
 
 function filterDish(filterToApply){
   let container = document.querySelector(".container");
@@ -207,28 +207,6 @@ function filterDish(filterToApply){
 }
 
 filterDish(null)
-
-/* List button */
-
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
-
-function filterFunction() {
-    var input, filter, ul, li, a, i;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    div = document.getElementById("myDropdown");
-    a = div.getElementsByTagName("a");
-    for (i = 0; i < a.length; i++) {
-        txtValue = a[i].textContent || a[i].innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            a[i].style.display = "";
-        } else {
-            a[i].style.display = "none";
-        }
-    }
-}
 
 /* Dark mode */
 
